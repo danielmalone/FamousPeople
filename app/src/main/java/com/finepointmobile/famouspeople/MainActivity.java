201481dcede63ee7aa6511ea100295380342cb9a
Daniel Malone <danieljmalone@gmail.com>
2017-10-28 16:03:25 -0500

    make app functional with ArrayList()

--- app/src/main/java/com/finepointmobile/famouspeople/MainActivity.java
+++ app/src/main/java/com/finepointmobile/famouspeople/MainActivity.java
@@ -4,14 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
     FloatingActionButton fab;
+    ArrayList<String> users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        recyclerView = findViewById(R.id.recycler_view);
+
+        users = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            users.add("Daniel # " + i);
+        }
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new UserAdapter(users);
+        recyclerView.setAdapter(adapter);
 
         fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
