8f7b7475aad5bc1371935a8c455c6847c7f2e188
Daniel Malone <danieljmalone@gmail.com>
2017-10-28 15:47:19 -0500

    more changes

---
+++ app/src/main/java/com/finepointmobile/famouspeople/CreateUser.java
@@ -0,0 +1,42 @@
+package com.finepointmobile.famouspeople;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by danielmalone on 10/28/17.
+ */
+
+public class CreateUser extends AppCompatActivity {
+
+    private static final String TAG = "CreateUser";
+
+    EditText firstName;
+    EditText lastName;
+    EditText email;
+    Button button;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_user);
+
+        firstName = findViewById(R.id.first_name);
+        lastName = findViewById(R.id.last_name);
+        email = findViewById(R.id.email);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: 10/28/17 Save to database
+                Log.d(TAG, "onClick: firstName: " + firstName.getText().toString());
+            }
+        });
+    }
+}
